0a80b553eb6e9346774ac626a15f8eb22cb4c0d8
Joshua Welch <oneadvent@joshuawelch.me>
2023-08-18 19:51:30 -0700

    adding structure to flask

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def helloWorld():
-    return'<h1>Hello World.</h1>'
+from app import app
+if __name__ == "__main__":
+    app.run()
