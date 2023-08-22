02c67d7b69a467e3bb121b21d36fa633dbe08f80
Joshua Welch <oneadvent@joshuawelch.me>
2023-08-21 21:44:13 -0700

    first try, don't run yet

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,28 @@
 from flask import Flask
-app = Flask(__name__)
-from app import views
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__, template_folder="templates")
+
+
+    # Load environment variables from .env
+    load_dotenv()
+
+    # Database configuration
+    db_user = os.getenv("DB_USER")
+    db_password = os.getenv("DB_PASSWORD")
+    db_host = os.getenv("DB_HOST")
+    db_name = os.getenv("DB_NAME")
+    db_uri = f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+
+    from . import views  # Import views here
+
+    return app
