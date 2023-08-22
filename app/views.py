ac994183a8c5b0cd559faa6c29a8ee60d4418a72
Joshua Welch <oneadvent@joshuawelch.me>
2023-08-21 23:03:39 -0700

    git (sic) rid of leftover comments

--- app/views.py
+++ app/views.py
@@ -1,8 +1,6 @@
 from flask import render_template
 from app import app
 
-# Define your view functions and routes here
-# For example:
 @app.route("/")
 def index():
     return render_template("index.html")
