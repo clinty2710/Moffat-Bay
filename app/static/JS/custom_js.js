18cb60cf23775a62a3a99c54a37f3f58b73a998b
Joshua Welch <oneadvent@joshuawelch.me>
2023-09-05 19:22:09 -0700

    update imagemapster and first pass at hilights

--- app/static/JS/custom_js.js
+++ app/static/JS/custom_js.js
@@ -40,4 +40,74 @@ $(document).ready(function() {
   // Initial update
   updateAreaCoordinates();
 });
-
+var xref = {
+  room_1a: "<b>Carrots</b> are delicious and may turn your skin orange!",
+  room_1b: "<b>Asparagus</b> is one of the first vegetables of the spring. " 
+      +"Being a dark green, it's great for you, and has interesting side effects.",
+  room_1c: "<b>Squash</b> is a winter vegetable, and not eaten raw too much. Is that really squash?",
+  room_1d: "<b>Red peppers</b> are actually the same as green peppers, they've just been left on "
+      +"the vine longer. Delicious when fire-roasted.",
+  room_1e: "Similar to red peppers, <b>yellow peppers</b> are sometimes sweeter.",
+  room_1f: "<b>Celery</b> is a fascinating vegetable. Being mostly water, it actually takes your body "
+      +"more calories to process it than it provides.",
+  room_1g: "<b>Cucumbers</b> are cool.",
+  room_1h: "<b>Broccoli</b> is like a forest of goodness in your mouth. And very good for you. "
+  +"Eat lots of broccoli!",
+  room_2a: "<b>Carrots</b> are delicious and may turn your skin orange!",
+  room_2b: "<b>Asparagus</b> is one of the first vegetables of the spring. " 
+  +"Being a dark green, it's great for you, and has interesting side effects.",
+  room_2c: "<b>Squash</b> is a winter vegetable, and not eaten raw too much. Is that really squash?",
+  room_2d: "<b>Red peppers</b> are actually the same as green peppers, they've just been left on "
+  +"the vine longer. Delicious when fire-roasted.",
+  room_2e: "Similar to red peppers, <b>yellow peppers</b> are sometimes sweeter.",
+  room_2f: "<b>Celery</b> is a fascinating vegetable. Being mostly water, it actually takes your body "
+  +"more calories to process it than it provides.",
+  room_2g: "<b>Cucumbers</b> are cool.",
+  room_2h: "<b>Broccoli</b> is like a forest of goodness in your mouth. And very good for you. "
+  +"Eat lots of broccoli!",
+};
+$(document).ready(function() {
+  var image = $('#hotel-image');
+  image.mapster(
+    {
+        fillOpacity: 0.4,
+        fillColor: "d42e16",
+        stroke: true,
+        strokeColor: "3320FF",
+        strokeOpacity: 0.8,
+        strokeWidth: 4,
+        singleSelect: true,
+        mapKey: 'name',
+        listKey: 'name',
+        onClick: function (e) {
+            // update text depending on area selected
+            $('#selections').html(xref[e.key]);
+  
+        },
+        showToolTip: true,
+        toolTipClose: ["tooltip-click", "area-click"],
+        areas: [
+            {
+                key: "room_1a",
+                fillColor: "ffffff"
+            },
+            {
+                key: "room_1b",
+                fillColor: "000000"
+            },
+            {
+                key: "room_1c",
+                fillColor: "000000"
+            },
+            {
+                key: "room_1d",
+                fillColor: "000000"
+            },
+            {
+                key: "room_1e",
+                strokeColor: "FFFFFF"
+            }
+            ]
+    });
+  
+});
