fd6cb91286224b131d50a6b964865916b56b4b82
Joshua Welch <oneadvent@joshuawelch.me>
2023-09-13 17:32:47 -0700

    docstring for models

--- app/models.py
+++ app/models.py
@@ -2,6 +2,21 @@ from app.db import db
 
 
 class User(db.Model):
+    """
+    Represents a user in the application.
+
+    Attributes:
+        uid (int): The unique identifier for the user (primary key).
+        Email (str): The user's email address (255 characters, not nullable).
+        First_name (str): The user's first name (255 characters).
+        Last_name (str): The user's last name (255 characters).
+        Phone_number (str): The user's phone number (up to 20 characters).
+        Password (str): The user's password (255 characters).
+        
+    Relationships:
+        reservations (list of Reservation): A list of reservations associated with the user.
+
+    """
     uid = db.Column(db.Integer, primary_key=True, autoincrement=True)
     Email = db.Column(db.String(255), nullable=False)
     First_name = db.Column(db.String(255))
@@ -11,6 +26,22 @@ class User(db.Model):
     reservations = db.relationship('Reservation', backref='user', lazy=True)
 
 class Reservation(db.Model):
+    """
+    Represents a reservation in the application.
+
+    Attributes:
+        rid (int): The unique identifier for the reservation (primary key).
+        start_date (Date): The start date of the reservation.
+        end_date (Date): The end date of the reservation.
+        room_number (str): The room number for the reservation (up to 10 characters).
+        num_of_guests (int): The number of guests for the reservation.
+        price (float): The price associated with the reservation.
+        user_id (int): The foreign key referencing the associated user.
+
+    Relationships:
+        user (User): The user associated with this reservation.
+
+    """
     rid = db.Column(db.Integer, primary_key=True, autoincrement=True)
     start_date = db.Column(db.Date)
     end_date = db.Column(db.Date)
